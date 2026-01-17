f500e0f01f05ff343778a05487bb1ba4147e7f93
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-01-17 11:24:53 +0900

    Implement main logic for Ice Cream Parlor solution

--- Ice-Cream-Parlor.cs
+++ Ice-Cream-Parlor.cs
@@ -1,1 +1,26 @@
-Ice-Cream-Parlor
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = Result.icecreamParlor(m, arr);
+
+            textWriter.WriteLine(String.Join(" ", result));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
